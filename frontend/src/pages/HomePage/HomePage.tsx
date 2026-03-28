a93cdb5f11d7c2c26047c3853fe6e04e1b391979
Sergii Stoiachko <119351759+SergiiSt@users.noreply.github.com>
2026-03-28 12:16:35 +0100

    add fetchUser by id

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,47 @@
 import HomeInfo from "../../components/HomeInfo/HomeInfo";
+import { useQuery } from "@tanstack/react-query";
+
+// import { User } from "@/types";
+
+type User = {
+  id: number;
+  username: string;
+};
+
+async function fetchUser(): Promise<User> {
+  const idFromStorage = localStorage.getItem("userId");
+  const response = await fetch(`/auth/${idFromStorage}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  return response.json();
+}
+
+function useGetUser() {
+  return useQuery({
+    queryKey: ["user"],
+    queryFn: fetchUser,
+  });
+}
 
 export default function HomePage() {
+  const { data: user, isLoading, error } = useGetUser();
+  console.log(user);
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error loading user data</p>;
+  }
   return (
     <>
       <h1 className="text-5xl pt-8">Home page</h1>
       <HomeInfo />
+      {!user ? null : <p>Welcome, {user.username}!</p>}
     </>
   );
 }
